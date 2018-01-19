711a49573f7c49f75268cca4cfb6c3d955cf67e5
RAMK15 <tcs15b023@iittp.ac.in>
2018-01-20 02:29:51 +0530

    Listview capable of opening other apps

--- app/src/main/java/com/android/ram/bug_reporter/HomeActivity.java
+++ app/src/main/java/com/android/ram/bug_reporter/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.android.ram.bug_reporter;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,16 +17,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    SharedPreferences app_list;
+    SharedPreferences.Editor app_list_editor;
+    private FancyAdapter mFancyAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //b=(Button) findViewById(R.id.test_button);
+        //t=(TextView)findViewById(R.id.textView2);
         setSupportActionBar(toolbar);
+        Toast.makeText(HomeActivity.this,"dsfjbjksdbhjebfhje",Toast.LENGTH_LONG);
+        final PackageManager pm = getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        //String s="";
+        final String []name=new String[packages.size()];
+        int i=0;
+        for (ApplicationInfo packageInfo : packages) {
+            //s=s+packageInfo.packageName+"\n";
+            name[i++]=packageInfo.packageName;
+        }
+        //t.setText(s);
+        ListView filelist=(ListView)findViewById(R.id.filelist);
+        mFancyAdapter=new FancyAdapter(name);
+        filelist.setAdapter(mFancyAdapter);
+
+        filelist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i=getPackageManager().getLaunchIntentForPackage(name[position]);
+                startActivity(i);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +70,8 @@ public class HomeActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent i=getPackageManager().getLaunchIntentForPackage("ai.replika.app");
+                startActivity(i);
             }
         });
 
@@ -97,5 +140,41 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private class FancyAdapter extends BaseAdapter {
+
+        private String[] mData;
+
+        public FancyAdapter(String[] data) {
+            mData = data;
+        }
+
+        @Override
+        public int getCount() {
+            return mData.length;
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mData[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView result;
+            if (convertView == null) {
+                result = (TextView) getLayoutInflater().inflate(R.layout.text_item, parent, false);
+            } else {
+                result = (TextView) convertView;
+            }
+            final String cheese = getItem(position);
+            result.setText(cheese);
+            return result;
+        }
     }
 }
