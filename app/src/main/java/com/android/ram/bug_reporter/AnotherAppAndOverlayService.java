c27b63fc0f54e3f6ef36d0bd639a694ae3830d9f
RAMK15 <tcs15b023@iittp.ac.in>
2018-01-21 09:43:58 +0530

    Scrapped the previous overlay service and created new one which now works(Opening of other apps has been deferred for later)

--- app/src/main/java/com/android/ram/bug_reporter/AnotherAppAndOverlayService.java
+++ app/src/main/java/com/android/ram/bug_reporter/AnotherAppAndOverlayService.java
@@ -1,91 +1,134 @@
 package com.android.ram.bug_reporter;
 
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class AnotherAppAndOverlayService extends Service {
-    HUDView mView;
-    public AnotherAppAndOverlayService() {
-    }
+public class AnotherAppAndOverlayService extends Service implements View.OnTouchListener, View.OnClickListener {
+
+    private View topLeftView;
+
+    private Button overlayedButton;
+    private float offsetX;
+    private float offsetY;
+    private int originalXPos;
+    private int originalYPos;
+    private boolean moving;
+    private WindowManager wm;
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(getBaseContext(),"onCreate", Toast.LENGTH_LONG).show();
-        mView = new HUDView(this);
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-                PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.RIGHT | Gravity.TOP;
-        params.setTitle("Load Average");
-        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-        wm.addView(mView, params);
 
-    }
+        wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Intent i=getPackageManager().getLaunchIntentForPackage(intent.getStringExtra("packagename"));
-        startActivity(i);
-        return super.onStartCommand(intent, flags, startId);
+        overlayedButton = new Button(this);
+        overlayedButton.setText("Overlay button");
+        overlayedButton.setOnTouchListener(this);
+        //overlayedButton.setAlpha(0.0f);
+        overlayedButton.setBackgroundColor(Color.BLACK);
+        overlayedButton.setOnClickListener(this);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.LEFT | Gravity.TOP;
+        params.x = 0;
+        params.y = 0;
+        wm.addView(overlayedButton, params);
+
+        topLeftView = new View(this);
+        WindowManager.LayoutParams topLeftParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+        topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
+        topLeftParams.x = 0;
+        topLeftParams.y = 0;
+        topLeftParams.width = 0;
+        topLeftParams.height = 0;
+        wm.addView(topLeftView, topLeftParams);
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getBaseContext(),"onDestroy", Toast.LENGTH_LONG).show();
-        if(mView != null)
-        {
-            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(mView);
-            mView = null;
+        if (overlayedButton != null) {
+            wm.removeView(overlayedButton);
+            wm.removeView(topLeftView);
+            overlayedButton = null;
+            topLeftView = null;
         }
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            float x = event.getRawX();
+            float y = event.getRawY();
+
+            moving = false;
+
+            int[] location = new int[2];
+            overlayedButton.getLocationOnScreen(location);
+
+            originalXPos = location[0];
+            originalYPos = location[1];
+
+            offsetX = originalXPos - x;
+            offsetY = originalYPos - y;
+
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            int[] topLeftLocationOnScreen = new int[2];
+            topLeftView.getLocationOnScreen(topLeftLocationOnScreen);
+
+            System.out.println("topLeftY="+topLeftLocationOnScreen[1]);
+            System.out.println("originalY="+originalYPos);
+
+            float x = event.getRawX();
+            float y = event.getRawY();
+
+            WindowManager.LayoutParams params = (WindowManager.LayoutParams) overlayedButton.getLayoutParams();
+
+            int newX = (int) (offsetX + x);
+            int newY = (int) (offsetY + y);
+
+            if (Math.abs(newX - originalXPos) < 1 && Math.abs(newY - originalYPos) < 1 && !moving) {
+                return false;
+            }
+
+            params.x = newX - (topLeftLocationOnScreen[0]);
+            params.y = newY - (topLeftLocationOnScreen[1]);
+
+            wm.updateViewLayout(overlayedButton, params);
+            moving = true;
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (moving) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Overlay button click event", Toast.LENGTH_SHORT).show();
+    }
+
 }
-
-class HUDView extends ViewGroup {
-    private Paint mLoadPaint;
-
-    public HUDView(Context context) {
-        super(context);
-        Toast.makeText(getContext(),"HUDView", Toast.LENGTH_LONG).show();
-
-        mLoadPaint = new Paint();
-        mLoadPaint.setAntiAlias(true);
-        mLoadPaint.setTextSize(10);
-        mLoadPaint.setARGB(255, 255, 0, 0);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawText("Hello World", 5, 15, mLoadPaint);
-    }
-
-    @Override
-    protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        //return super.onTouchEvent(event);
-        Toast.makeText(getContext(),"onTouchEvent", Toast.LENGTH_LONG).show();
-        return true;
-    }
-}
-
