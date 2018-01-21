b7edd2e083a1447bc1cef65633dff702429628f1
RAMK15 <tcs15b023@iittp.ac.in>
2018-01-21 20:54:04 +0530

    made the buttons function.able to get the contents of any activity using accessibility api.

--- app/src/main/java/com/android/ram/bug_reporter/GlobalActionBarService.java
+++ app/src/main/java/com/android/ram/bug_reporter/GlobalActionBarService.java
@@ -1,15 +1,24 @@
 package com.android.ram.bug_reporter;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Path;
 import android.graphics.PixelFormat;
+import android.media.AudioManager;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class GlobalActionBarService extends AccessibilityService {
     FrameLayout mLayout;
@@ -30,6 +39,9 @@ public class GlobalActionBarService extends AccessibilityService {
         LayoutInflater inflater = LayoutInflater.from(this);
         inflater.inflate(R.layout.action_bar, mLayout);
         wm.addView(mLayout, lp);
+        configurePowerButton();
+        configureScrollButton();
+        configureVolumeButton();
     }
 
     @Override
@@ -42,4 +54,51 @@ public class GlobalActionBarService extends AccessibilityService {
 
     }
 
+    private void configurePowerButton() {
+        Button powerButton = (Button) mLayout.findViewById(R.id.power);
+        powerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
+            }
+        });
+    }
+    private AccessibilityNodeInfo findScrollableNode(AccessibilityNodeInfo root) {
+        Deque<AccessibilityNodeInfo> deque = new ArrayDeque<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            AccessibilityNodeInfo node = deque.removeFirst();
+            if (node.getActionList().contains(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD)) {
+                return node;
+            }
+            for (int i = 0; i < node.getChildCount(); i++) {
+                deque.addLast(node.getChild(i));
+            }
+        }
+        return null;
+    }
+
+    private void configureScrollButton() {
+        Button scrollButton = (Button) mLayout.findViewById(R.id.scroll);
+        scrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccessibilityNodeInfo scrollable = findScrollableNode(getRootInActiveWindow());
+                if (scrollable != null) {
+                    scrollable.performAction(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD.getId());
+                }
+            }
+        });
+    }
+    private void configureVolumeButton() {
+        Button volumeUpButton = (Button) mLayout.findViewById(R.id.volume_up);
+        volumeUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                        AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+            }
+        });
+    }
 }
